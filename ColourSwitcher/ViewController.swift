2fb4e78f4e2c912c16160ecee1d9036c25eff65a
Anna Lavrova <anna.laurier99@gmail.com>
2024-02-01 20:37:50 +0300

    HM Swither

--- ColourSwitcher/ViewController.swift
+++ ColourSwitcher/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var yellowLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+
+    @IBOutlet weak var redLabelCount: UILabel!
+    @IBOutlet weak var yellowLabelCount: UILabel!
+    @IBOutlet weak var greenLabelCount: UILabel!
+
+    @IBOutlet weak var redColorSlider: UISlider!
+    @IBOutlet weak var yellowColorSlider: UISlider!
+    @IBOutlet weak var greenColorSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        redLabel.text = "Red"
+        yellowLabel.text = "Yellow"
+        greenLabel.text = "Green"
+
+        colorView.layer.cornerRadius = 15
+
+        setSlider(redColorSlider)
+        setSlider(yellowColorSlider)
+        setSlider(greenColorSlider)
+
+        redLabelCount.text = "0.00"
+        yellowLabelCount.text = "0.00"
+        greenLabelCount.text = "0.00"
     }
 
+    @IBAction func getNumberOfRed() {
+        redLabelCount.text = String(format: "%.2f", redColorSlider.value)
+        updateColor()
+    }
 
+    @IBAction func getNumberOfYellow() {
+        yellowLabelCount.text = String(format: "%.2f", yellowColorSlider.value)
+        updateColor()
+    }
+
+    @IBAction func getNumberOfGreen() {
+        greenLabelCount.text = String(format: "%.2f", greenColorSlider.value)
+        updateColor()
+    }
+
+    func setSlider(_ slider: UISlider) {
+        slider.minimumValue = 0
+        slider.maximumValue = 255
+        slider.value = 0
+    }
+
+    func updateColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value) / 255,
+            green: CGFloat(greenColorSlider.value) / 255,
+            blue: CGFloat(yellowColorSlider.value) / 255,
+            alpha: 1.0
+        )
+    }
 }
-
